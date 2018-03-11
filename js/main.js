f890dae096dfdb20c79dc338e9e6dda9b834831f
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-11 19:28:35 +0000

    footer and book button

--- js/main.js
+++ js/main.js
@@ -1,52 +1,12 @@
-// // Main carousel
-// const mainArrows = document.querySelectorAll('.arrow');
-// const mainBackgroundImages = document.querySelectorAll('.main_banner .images img');
-// const indexOfMainBackgroundImages = mainBackgroundImages.length - 1;
-// let mainCarouselCounter = 0;
-// let appPageCounter = 0;
-//
-// for (let item of mainArrows) {
-// 	item.addEventListener('click', () => {
-// 		mainImageArrows(item);
-// 	})
-// }
-//
-// function mainImageArrows(item) {
-// 	if (item.classList.contains('back_arrow')) {
-// 		decreaseMainImageCounter();
-// 	}
-// 	if (item.classList.contains('next_arrow')) {
-// 		increaseMainImageCounter();
-// 	}
-// }
-//
-// function decreaseMainImageCounter() {
-// 	if (mainCarouselCounter) {
-// 		mainCarouselCounter--;
-// 	} else {
-// 		mainCarouselCounter = indexOfMainBackgroundImages;
-// 	}
-// 	showMainImage();
-// }
-//
-// function increaseMainImageCounter() {
-// 	if (mainCarouselCounter < indexOfMainBackgroundImages) {
-// 		mainCarouselCounter++;
-// 	} else {
-// 		mainCarouselCounter = 0;
-// 	}
-// 	showMainImage();
-// }
-//
-// function showMainImage() {
-// 	for (let item of mainBackgroundImages) {
-// 		item.classList.remove('active');
-// 	}
-// 	mainBackgroundImages[mainCarouselCounter].classList.add('active');
-// }
-//
-// (() => {
-// 	setInterval(increaseMainImageCounter, 10000);
-// })();
-// // =========================================
-"use strict";
+'use strict';
+
+var bookingButton = document.querySelector('.book_journey');
+
+window.addEventListener('scroll', function () {
+	// console.log(window.scrollY);
+	if (window.scrollY > 400) {
+		bookingButton.classList.add('active');
+	} else {
+		bookingButton.classList.remove('active');
+	}
+});

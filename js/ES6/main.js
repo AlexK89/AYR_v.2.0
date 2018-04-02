a1ee68c43523ac44d8d93cef7240d0bdc971bb0a
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-02 21:21:26 +0100

    navigation dropdown closing effect

--- js/ES6/main.js
+++ js/ES6/main.js
@@ -1,1 +1,16 @@
+let showDropdownMenu = document.querySelector('.menu');
+let menuButton = document.querySelector('.dropdown_menu img');
 
+menuButton.addEventListener('click', (ev) => {
+	ev.stopPropagation();
+
+	if (showDropdownMenu.style.height === '320px') {
+		showDropdownMenu.style.height = '0';
+	} else {
+		showDropdownMenu.style.height = '320px';
+	}
+});
+
+document.querySelector('body').addEventListener('click', function () {
+	showDropdownMenu.style.height = '0';
+});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';
+
+var showDropdownMenu = document.querySelector('.menu');
+var menuButton = document.querySelector('.dropdown_menu img');
+
+menuButton.addEventListener('click', function (ev) {
+	ev.stopPropagation();
+
+	if (showDropdownMenu.style.height === '320px') {
+		showDropdownMenu.style.height = '0';
+	} else {
+		showDropdownMenu.style.height = '320px';
+	}
+});
+
+document.querySelector('body').addEventListener('click', function () {
+	showDropdownMenu.style.height = '0';
+});
